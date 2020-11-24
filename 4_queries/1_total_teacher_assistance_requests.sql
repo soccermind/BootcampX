ccfb13c791432659089fcc98effa692a39098635
Alberto Olvera Sotres <mty2yyz@gmail.com>
2020-11-24 17:20:59 -0500

    Add 4_queries activity

---
+++ 4_queries/1_total_teacher_assistance_requests.sql
@@ -0,0 +1,6 @@
+SELECT COUNT(*) as total_assistances, name
+FROM assistance_requests
+JOIN teachers
+ON teacher_id = teachers.id
+WHERE name = 'Waylon Boehm'
+GROUP BY name;
---
+++ 4_queries/2_total_student_assistance_requests.sql
@@ -0,0 +1,6 @@
+SELECT COUNT(*) as total_assistances, name
+FROM assistance_requests
+JOIN students
+ON student_id = students.id
+WHERE name = 'Elliot Dickinson'
+GROUP BY name;
